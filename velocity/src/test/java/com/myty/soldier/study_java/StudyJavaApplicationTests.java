ca00770e22c5e7cd413827992c1da7ad991d0f65
Yoshiki Takamatsu <yoshi008t@gmail.com>
2021-03-06 23:15:34 +0900

    Velocity sample

---
+++ velocity/src/test/java/com/myty/soldier/study_java/StudyJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myty.soldier.study_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
