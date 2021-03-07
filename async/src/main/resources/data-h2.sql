fe326d52ca560fd45e3e0356d071e13bb962eb81
Yoshiki Takamatsu <yoshi008t@gmail.com>
2021-03-08 00:08:54 +0900

    Async sample (combination of async annotation and completablefuture)

---
+++ async/src/main/resources/data-h2.sql
@@ -0,0 +1,5 @@
+INSERT INTO sample_table (text) values ('sampletext1');
+INSERT INTO sample_table (text) values ('sampletext2');
+INSERT INTO sample_table (text) values ('sampletext3');
+INSERT INTO sample_table (text) values ('sampletext4');
+INSERT INTO sample_table (text) values ('sampletext5');
