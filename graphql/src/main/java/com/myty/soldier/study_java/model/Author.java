2f9614529bcb636ca24a9003557c3e98b72b7e05
Yoshiki Takamatsu <yoshi008t@gmail.com>
2021-03-26 23:03:39 +0900

    Graphql sample

---
+++ graphql/src/main/java/com/myty/soldier/study_java/model/Author.java
@@ -0,0 +1,14 @@
+package com.myty.soldier.study_java.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Author {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+
+}
